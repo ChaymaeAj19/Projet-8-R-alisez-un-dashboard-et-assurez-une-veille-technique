982e6111fdc9745e4108d55d24b3b6b890aeb9a1
ChaymaeAj19 <ajjajichaymae19@gmail.com>
2025-05-22 22:31:26 +0200

    ajout du main

---
+++ main.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Dashboard Crédit Client", layout="wide")
+
+st.title("Dashboard Crédit Client")
+st.write("Bienvenue ! Sélectionnez un client pour afficher ses informations.")
