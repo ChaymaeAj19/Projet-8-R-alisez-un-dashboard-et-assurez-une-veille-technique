43215dbbde0c3a7e2509ce87b7437edd192eb986
ChaymaeAj19 <ajjajichaymae19@gmail.com>
2025-06-04 21:38:32 +0200

    modif du fichier man.py

--- main.py
+++ main.py
@@ -16,31 +16,37 @@ if client_id:
         st.error(f"Erreur : {data['error']}")
     else:
         st.subheader("Résultat de l'analyse")
-        score = data["score"]
-        prediction = data["prediction"]
 
-        st.metric(label="Score Crédit", value=f"{score:.2f}")
-        st.metric(label="Décision", value=prediction)
+        # Affichage du score de défaut (en pourcentage)
+        proba = data.get("proba_default", None)
+        if proba is not None:
+            score_percent = round(proba * 100, 2)
+            st.metric(label="Probabilité de défaut", value=f"{score_percent} %")
 
-        # Jauge
-        fig = go.Figure(go.Indicator(
-            mode="gauge+number+delta",
-            value=score * 100,
-            delta={'reference': 50},
-            gauge={
-                'axis': {'range': [0, 100]},
-                'bar': {'color': "green" if score >= 0.5 else "red"},
-                'steps': [
-                    {'range': [0, 50], 'color': 'rgba(255,0,0,0.2)'},
-                    {'range': [50, 100], 'color': 'rgba(0,255,0,0.2)'}
-                ],
-            },
-            title={'text': "Score (%)"}
-        ))
-        st.plotly_chart(fig, use_container_width=True)
+            # Jauge
+            fig = go.Figure(go.Indicator(
+                mode="gauge+number",
+                value=score_percent,
+                gauge={
+                    'axis': {'range': [0, 100]},
+                    'bar': {'color': "red" if score_percent > 50 else "green"},
+                    'steps': [
+                        {'range': [0, 50], 'color': 'rgba(0,255,0,0.2)'},
+                        {'range': [50, 100], 'color': 'rgba(255,0,0,0.2)'}
+                    ]
+                },
+                title={'text': "Score de risque (%)"}
+            ))
+            st.plotly_chart(fig, use_container_width=True)
+        else:
+            st.warning("Aucune probabilité de défaut trouvée.")
+
 
         # Affichage des caractéristiques client
         st.subheader("Caractéristiques du client")
         features = data["client_features"]
         df_client = pd.DataFrame.from_dict(features, orient="index", columns=["Valeur"])
         st.dataframe(df_client)
+
+
+
