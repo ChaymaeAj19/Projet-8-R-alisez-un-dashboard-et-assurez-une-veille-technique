1138aca05586624c8441b5851dc807afb2ba6d33
ChaymaeAj19 <ajjajichaymae19@gmail.com>
2025-06-04 21:35:32 +0200

    Ajout de utils.py

--- utils.py
+++ utils.py
@@ -4,7 +4,9 @@ API_URL = "https://projet-7-implementation.onrender.com/predict"
 
 def get_prediction(client_id):
     try:
-        response = requests.get(API_URL, params={"client_id": client_id})
+        payload = {"SK_ID_CURR": int(client_id)}
+        headers = {"Content-Type": "application/json"}
+        response = requests.post(API_URL, json=payload, headers=headers)
         response.raise_for_status()
         return response.json()
     except Exception as e:
